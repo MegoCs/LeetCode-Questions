23a755fffb0a653da2bcd05ff8df2fc26ece1757
Ahmed Kamel <Ahmed.Kamel@linkdev.com>
2020-05-03 04:55:57 +0200

    Add project files.

---
+++ LeetCode-Questions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCode_Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
