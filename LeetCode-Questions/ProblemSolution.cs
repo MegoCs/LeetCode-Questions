c641e3e34367785c0346975d3d8385b11d20017a
Ahmed Kamel <Ahmed.Kamel@linkdev.com>
2020-12-26 06:05:51 +0200

    Problem =>   Find Numbers with Even Number of Digits

--- LeetCode-Questions/ProblemSolution.cs
+++ LeetCode-Questions/ProblemSolution.cs
@@ -10,27 +10,19 @@
             //Convert result object to problem output type
             var expectedResult = (int)expectedObj;
 
-            var result = RemoveDuplicates(input);
+            var result = FindNumbers(input);
 
             return result.Equals(expectedResult);
         }
-        public int RemoveDuplicates(int[] nums)
+        public int FindNumbers(int[] nums)
         {
-            int lastUnique=0;
-            if (nums.Length == 0)
-                return 0;
-            for (int i = 1; i < nums.Length; i++)
+            int result = 0;
+            for (int i = 0; i < nums.Length; i++)
             {
-                if (nums[i] != nums[i - 1])
-                {
-                    while (i < nums.Length - 1 && nums[i]==nums[i+1])
-                    {
-                        i++;
-                    }
-                    nums[++lastUnique] = nums[i];
-                }
+                if (nums[i].ToString().Length % 2 == 0)
+                    result++;
             }
-            return lastUnique+1;
+            return result;
         }
     }
 }
--- LeetCode-Questions/SolutionTest.cs
+++ LeetCode-Questions/SolutionTest.cs
@@ -20,7 +20,7 @@ namespace LeetCode_Questions
         private static readonly Dictionary<object, object> Inputs = new Dictionary<object, object>()
         {
             {
-               new int[] {0,0,1,1,1,2,2,3,3,4},5
+               new int[] {555,901,482,1771},1
             }
         };
     }
