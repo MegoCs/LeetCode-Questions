126d7eef4dcd85908b371011174cdfaf1c999a35
Ahmed Magdy <MegoCs@Hotmail.com>
2021-01-07 02:16:20 +0200

    Problem -> HeightChecker

--- LeetCode-Questions/ProblemSolution.cs
+++ LeetCode-Questions/ProblemSolution.cs
@@ -9,58 +9,28 @@ namespace LeetCode_Questions
         public bool ValidateSolutionAgainst(object inputObj, object expectedObj)
         {
             //Convert Object to problem real type
-            var input = (Tuple<int[],int>)inputObj;
+            var input = (int[])inputObj;
 
             //Convert result object to problem output type
             var expectedResult = (int)expectedObj;
 
-            var result = isPairedN(input.Item1, input.Item2);
+            var result = HeightChecker(input);
 
             return result.Equals(expectedResult);
         }
 
-        public int isPairedN(int[] a, int n) {
-
-            if (a.Length - 1 + a.Length - 2 < n || a.Length<2 || n<=0)
-                return 0;
-
-            for (int i = 0; i < a.Length; i++)
-            {
-                for (int j = i; j < a.Length; j++)
-                {
-                    if (a[i] + a[j] == n && i + j == n)
-                        return 1;
-                }
-            }
-            return 0;
-        }
-
-        public int is123Array(int[] a)
+        public int HeightChecker(int[] heights)
         {
-            int valid = 0;
-            for (int i = 0; i < a.Length; i++)
+            var unchanged = (int[])heights.Clone();
+            heights = heights.OrderBy(x => x).ToArray();
+            int result = 0;
+            for (int i = 0; i < heights.Length; i++)
             {
-                if (i + 2 < a.Length && a[i] == 1 && a[i + 1] == 2 && a[i + 2] == 3)
-                {
-                    i += 2;
-                    valid = 1;
-                }
-                else
-                {
-                    return 0;
-                }
+                if (heights[i] != unchanged[i])
+                    result++;
             }
-            return valid;
+            return result;
         }
-        public int hasNValues(int[] a, int n)
-        {
-            HashSet<int> uniqueSet = new HashSet<int>();
-            for (int i = 0; i < a.Length; i++)
-            {
-                if (!uniqueSet.Contains(a[i]))
-                    uniqueSet.Add(a[i]);
-            }
-            return uniqueSet.Count == n ? 1 : 0;
-        }
+
     }
 }
--- LeetCode-Questions/SolutionTest.cs
+++ LeetCode-Questions/SolutionTest.cs
@@ -21,29 +21,14 @@ namespace LeetCode_Questions
         private static readonly Dictionary<object, object> Inputs = new Dictionary<object, object>()
         {
             {
-               Tuple.Create(new int []{1, 4, 1, 4, 5, 6},5),1
+               new int []{ 1,1,4,2,1,3},3
             },
             {
-               Tuple.Create(new int []{1, 4, 1, 4, 5, 6},6),1
+               new int []{5,1,2,3,4},5
             },
             {
-               Tuple.Create(new int []{0, 1, 2, 3, 4, 5, 6, 7, 8},6),1
-            },
-            {
-               Tuple.Create(new int []{1, 4, 1},5),0
-            },
-            {
-               Tuple.Create(new int []{8, 8, 8, 8, 7, 7, 7},15),0
-            },
-            {
-               Tuple.Create(new int []{8, -8, 8, 8, 7, 7, -7},-15),0
-            },
-            {
-               Tuple.Create(new int []{3},3),0
-            },
-            {   
-               Tuple.Create(new int []{},0),0
-            },
+               new int []{ 1,2,3,4,5},0
+            }
         };
     }
 }
